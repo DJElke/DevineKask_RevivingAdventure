e2995df7a9c9bcec69dc4ed2dd66f250ba1f952c
Camille <camille.buyle@student.howest.be>
2020-06-23 14:58:43 +0200

    Css register and login

--- src/view/statisch/onboarding.php
+++ src/view/statisch/onboarding.php
@@ -1,9 +1,9 @@
 <div class="onboarding1">
 <h1>Recreate your vacation in a card game</h1>
 <p>Want to revive that one amazin vacation you had with your friends, but think making a photo album is just too boring?</p>
-<img src="assets/illustrations/cards.svg">
+<img class="onboarding_image" src="assets/illustrations/cards.svg">
 
-<div>
+<div class="onboarding_dots">
   <span class="dot"></span>
   <span class="open-dot"></span>
   <span class="open-dot"></span>
@@ -14,10 +14,10 @@
 <div class="hide onboarding2">
 <h1>Be the game MASTER</h1>
 <p>if you are the "game master" you are in charge of uploading the photo's.</p>
-<img src="assets/illustrations/dice.svg">
+<img class="onboarding_image" src="assets/illustrations/dice.svg">
 <p>Invite your friends so they can review them and add hilarious content to them</p>
 
-<div>
+<div class="onboarding_dots">
   <span class="dot"></span>
   <span class="dot"></span>
   <span class="open-dot"></span>
@@ -27,10 +27,10 @@
 <div class="hide onboarding3">
 <h1>Wait for arrival</h1>
 <p>Did you have fun making your personalized game?</p>
-<img src="assets/illustrations/box.svg">
+<img class="onboarding_image" src="assets/illustrations/box.svg">
 <p>Order it and plan out an amazing evening with the friends you made it with. But be prepared for all kind of suprises!</p>
 
-<div>
+<div class="onboarding_dots">
   <span class="dot"></span>
   <span class="dot"></span>
   <span class="dot"></span>
--- src/view/statisch/register.php
+++ src/view/statisch/register.php
@@ -1,10 +1,10 @@
 <div class="loginRegister">
   <div>
-    <p>Hello <b>stranger!</b></p>
-    <p>Let's go on an adventure</p>
+    <p class="title_register">Hello <b>stranger</b>!</p>
+    <p class="subtitle_register">Let's go on an adventure ...</p>
   </div>
 
-  <div>
+  <div class="loginandregister_button">
     <a class="register--button">REGISTER</a>
     <a class="login--button">LOGIN</a>
   </div>
@@ -12,72 +12,77 @@
 
 <div class="hide personaliseAccount">
   <div>
-    <p>Hello <b>stranger!</b></p>
-    <p>Let's go on an adventure</p>
+    <p class="title_register">Hello <b>stranger!</b></p>
+    <p class="subtitle_register">Let's go on an adventure</p>
   </div>
 
-    <div>
+    <div class="register--name">
       <label for="ptName">What's your name good-looking?</p>
-      <input placeholder="e.g. Handsome Harry" id="ptName" name="ptName">
+      <input class="input_register" placeholder="e.g. Handsome Harry" id="ptName" name="ptName">
     </div>
 
-    <div>
+    <div class="register--color">
       <p>What's your favorite color</p>
       <div class="color--options">
-        <div><input type="radio" class="color--option" id="blue" name="blue" value="blue"><label for="blue" ><span class="blue--dot"><p>blue</p></label></div>
-        <div><input type="radio" class="color--option" id="purple" name="purple" value="purple"><label for="purple" ><span class="purple--dot"><p>purple</p></label></div>
-        <div><input type="radio" class="color--option" id="orange" name="orange" value="orange"><label for="orange" ><span class="orange--dot"><p>orange</p></label></div>
+        <div><input type="checkbox" class="color--option" id="blue" name="blue" value="blue"><label for="blue" ><span class="blue--dot"><p class="namecolor_register">blue</p></label></div>
+        <div><input type="checkbox" class="color--option" id="purple" name="purple" value="purple"><label for="purple" ><span class="purple--dot"><p class="namecolor_register">purple</p></label></div>
+        <div><input type="checkbox" class="color--option" id="orange" name="orange" value="orange"><label for="orange" ><span class="orange--dot"><p  class="namecolor_register">orange</p></label></div>
       </div>
     </div>
 
-  <div>
+  <div class="register--avatar">
       <p >Choose your avatar</p>
         <div class="color--options">
-          <div><input type="radio"  id="blue" name="ptAvatar" value="lemon"><label for="lemon" ><img src="assets/user-icons/icon-citrus.svg"></label></div>
-          <div><input type="radio"  id="purple" name="ptAvatar" value="cocktail"><label for="cocktail" ><img src="assets/user-icons/icon-cocktail.svg"></label></div>
-          <div><input type="radio"  id="orange" name="ptAvatar" value="backpack"><label for="backpack" ><img src="assets/user-icons/icon-backpack.svg"></label></div>
-          <div><input type="radio"  id="orange" name="ptAvatar" value="melon"><label for="backpack" ><img src="assets/user-icons/icon-watermelon.svg"></label></div>
-
+          <div><input type="checkbox"  id="blue" name="ptAvatar" value="lemon"><label for="lemon" ><img src="assets/user-icons/icon-citrus.svg"></label></div>
+          <div><input type="checkbox"  id="purple" name="ptAvatar" value="cocktail"><label for="cocktail" ><img src="assets/user-icons/icon-cocktail.svg"></label></div>
+          <div><input type="checkbox"  id="orange" name="ptAvatar" value="backpack"><label for="backpack" ><img src="assets/user-icons/icon-backpack.svg"></label></div>
+          <div><input type="checkbox"  id="orange" name="ptAvatar" value="melon"><label for="backpack" ><img src="assets/user-icons/icon-watermelon.svg"></label></div>
         </div>
     </div>
-    <div>
+    <div class="register--nextbotton">
     <a class="personaliseAccount--button">NEXT</a>
   </div>
 </div>
 
 <div class=" hide personalData">
-  <p>Hello <span class="blue">Elke!</span><p>
-  <p>Let's set up your account<p>
-  <div>
-    <label for="ptEmail">
-    <input type=email placeholder="e.g. harry@handsome.org" name="ptEmail">
+  <p class="title_register">Hello <b><span class="blue">Elke!</span></b><p>
+  <p class="subtitle_register">Let's set up your account<p>
+  <div class="register--final--input">
+    <label for="ptEmail">E-mail:<br></label>
+    <input class="input_register" type=email placeholder="e.g. harry@handsome.org" name="ptEmail">
   </div>
-  <div>
-    <label for="ptPassword">
-    <input type=password placeholder="I can lift 200lbs on bench press" name="ptPassword">
+  <div class="register--final--input">
+    <label for="ptPassword">Strong password:<br></label>
+    <input class="input_register" type=password placeholder="iamsohandsome321" name="ptPassword">
   </div>
-  <div>
-    <label for="ptPasswordRe">
-    <input type=password placeholder="I can lift 200lbs on bench press" name="ptPasswordRe">
+  <div class="register--final--input">
+    <label for="ptPasswordRe">Re-type password:<br></label>
+    <input class="input_register" type=password placeholder="iamsohandsome321" name="ptPasswordRe">
   </div>
-  <div>
-    <a href="index.php?page=tutorial">REGISTER</a>
+  <div class="register--finishedbotton">
+    <a class="registerFinished--button" href="index.php?page=tutorial">REGISTER</a>
   </div>
 </div>
 
 <div class="hide login" >
-  <h1>Welcome back!</h1>
+  <h1 class="title_register">Welcome back!</h1>
 
-  <div>
-    <label>E-mail</label>
-    <input type=email placeholder="e.g. harry@handsome.org">
+  <div class="register--final--input">
+    <label>E-mail:</label>
+    <input class="input_register" type=email placeholder="e.g. harry@handsome.org">
   </div>
-  <div>
-    <label>Password</label>
-    <input type=password>
-    <p>forgot you  password</p>
+  <div class="register--final--input">
+    <label>Password:</label>
+    <input class="input_register" type=password placeholder="iamsohandsome321">
+    <p class="login--forgotPassword">Forgot your password?</p>
   </div>
-  <img src="assets/illustrations/plane-gray.svg">
-  <a href="index.php?page=home">LOGIN</a>
-  <a class="register--button">I have no account yet</a>
+  <div class="loginImage">
+    <img class="login--image" src="assets/illustrations/plane-gray.svg">
+  </div>
+  <div class="loginFinished">
+    <a class="loginFinished--button" href="index.php?page=home">LOGIN</a>
+  </div>
+  <div class="nologin">
+    <a class="nologin--button" href="index.php?page=register">I have no account yet</a>
+  </div>
 </div>

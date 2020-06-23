ddc19b04dba4c0b3a4c805c7e13e5c07eefd331f
Elke De Jonckere <elke.dejonckere@gmail.com>
2020-06-23 21:35:01 +0200

    css station 2 & 3

--- src/view/station/ownerStation2.php
+++ src/view/station/ownerStation2.php
@@ -6,16 +6,19 @@
 </div>
 
 <div class="tab">
-<a href="index.php?page=home">
-  <span class="close">&#10005;</span>
-</a>
-<header>
-  <h1>Station two </h1>
-  <h2>Item cards for each player</h2>
-</header>
-<img src='assets/illustrations/backpack.svg'>
-<p>Add some items/typicle situations for your friends!<p>
-<button type="button" class="nextBtn" >START</button>
+  <div class="station__grid">
+    <header class="stationHeader">
+    <h1 class="blue">Station two 
+      <a href="index.php?page=home">
+        <span class="close">&#10005;</span>
+      </a>
+    </h1>
+    <p class="text--medium red text--margintop">Item cards for each player</p>
+    </header>
+    <img class="stationIntro__img stationIntro__img--padding" src='assets/illustrations/backpack.svg'>
+    <p class="stationIntro__text text--medium">Add some items/typical situations for your friends!</p>
+    <button type="button" class="stationIntro__next nextBtn secondaryButton" >START</button>
+  </div>
 </div>
 
 <form class="itemCardForm" action="index.php?page=ownerStation2&amp;id=<?php echo $vacationId;?>" method="post" enctype="multipart/form-data">
@@ -29,31 +32,49 @@
 
   <?php foreach($participants as $participant): ?>
     <div class="tab">
-    <a class="create-item--close">
-        <span class="close">&#10005;</span>
-      </a>
-      <p>Item cards for <?php echo $participant['name'] ?></p>
-      <div class="itemCardFrom__imagesholder">
-        <?php for($e = 1; $e <= $demoGame; $e++): ?>
-        <label for="ptImage<?php echo $i;?>" class="itemCard__output__label">
-          <img class="output"/>
-          <input type="file" accept="image/*" class="itemCardForm__img" name="itemImage<?php echo $i;?>" id="ptImage<?php echo $i;?>">
-        </label>
-        <?php endfor; ?>
-      </div>
-      <p class="error"></p>
-      <div>
-        <button type="button" class="prevBtn">PREVIOUS</button>
-        <button type="button" class="nextBtn"><?php echo $participant['id'] ?>/<?php echo count($participants) ?> NEXT</button>
-      </div>
+      <div class=station__grid>
+        <!-- HEADER -->
+        <div class="stationHeader">
+          <h1 class="blue">Item cards for <span class="red"><?php echo $participant['name'] ?></span>
+            <a class="create-item--close">
+              <span class="close">&#10005;</span>
+            </a>
+          </h1>
+        </div>
+
+         
+         <div class="img--input fileInput__wrapper">
+            <?php for($e = 1; $e <= $actualGame; $e++): ?>
+              <div class="stationFileInput">
+              <img class="hide output--small"/>
+              <label class="custom-file-upload custom-file-upload--small" for="ptImage<?php echo $i;?>">+</label>
+              <input type="file" accept="image/*" class="itemCardForm__img" name="itemImage<?php echo $i;?>" id="ptImage<?php echo $i;?>">
+            </label>
+              </div>
+            <?php endfor; ?>
+          </div>
+          <p class="error"></p>
+      
+          <!-- NEXT BUTTON -->
+          <div class="stationButtons">
+            <button type="button" class="nextBtn secondaryButton"><?php echo $participant['id'] ?>/<?php echo count($participants) ?> NEXT</button>
+          </div>
+
+        </div>
     </div>
     <?php $i = $i + 1 ?> 
   <?php endforeach; ?> 
   <?php unset($i); ?>
 
   <div class="tab">
-    <p>You are all done!</p>
-    <button type="submit" name="action" value="add">SUBMIT</button>
+    <div class="station__grid">
+      <div class="stationDone">
+        <h1 class="blue">You are all done!</h1>
+        </br>
+        <p>I bet you can't wait for their review! &#128540;</p>
+        <button type="submit" name="action" value="add" class="primaryButton">SUBMIT</button>
+      </div>
+    </div>
   </div>
 </form>
 
